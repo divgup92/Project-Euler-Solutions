f8d5dd2d11db807ce6393ab157b29a5c0d427d5c
Divya <erdevya@gmail.com>
2019-06-30 21:28:47 +0530

    Adding few more solutions

---
+++ src/utils/Factors.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factors {
+
+	public static List<Long> find(long number) {
+		List<Long> factors = new ArrayList<>();
+		factors.add(1l);
+		factors.add(number);
+		for(long i=2;i<number;i++) {
+			if(number%i==0)
+				factors.add(i);
+		}
+		return factors;
+	}
+	
+	public static int findSize(long number) {
+		int count = 2;
+		for(int i=2;i<number;i++) {
+			if(number%i==0)
+				count++;
+		}
+		return count;
+	}
+}
---
+++ src/utils/TriangleNumbers.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TriangleNumbers {
+
+	public static List<Long> triangleNumbers(long n) {
+		List<Long> nos = new ArrayList<>();
+		for (long i = 1; (i * (i + 1)) / 2 <= n; i++) {
+			nos.add((i * (i + 1)) / 2);
+		}
+		return nos;
+	}
+	
+	public static List<Long> triangleNumbersInRange (long n1, long n2) {
+		List<Long> nos = new ArrayList<>();
+		long i = (-1 + (long)Math.sqrt(1+(8*n1)) )/2;
+		for (; (i * (i + 1)) / 2 <= n2; i++) {
+			nos.add((i * (i + 1)) / 2);
+		}
+		return nos;
+	}
+}
